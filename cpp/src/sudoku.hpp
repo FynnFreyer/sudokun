6c772cec84b45166667e819ce0326659e636eb4e
Fynn Freyer <fynn.freyer@protonmail.com>
2022-08-11 00:38:10 +0200

    add sudoku.hpp

---
+++ cpp/src/sudoku.hpp
@@ -0,0 +1,24 @@
+#ifndef SUDOKU_HPP
+#define SUDOKU_HPP
+
+#include <vector>
+#include <string>
+
+class Sudoku {
+    std::vector<int> candidates[9][9];
+    int data[9][9];
+
+    std::vector<int> get_row_values(int row_index);
+    std::vector<int> get_col_values(int col_index);
+    std::vector<int> get_box_values(int x, int y);
+
+    std::vector<int> get_row_candidates(int row_index);
+    std::vector<int> get_col_candidates(int col_index);
+    std::vector<int> get_box_candidates(int x, int y);
+
+public:
+    Sudoku(std::string file_path);
+    void solve();
+};
+
+#endif // SUDOKU_HPP
