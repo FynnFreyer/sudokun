64f9e54dacbf75d843cc3a3d37933be59a7e1acd
Fynn Freyer <fynn.freyer@protonmail.com>
2022-08-12 20:07:06 +0200

    actually read input file from command line and change cli parsing wrt usage printing also add some sanity checks while parsing

--- cpp/src/main.cpp
+++ cpp/src/main.cpp
@@ -8,24 +8,23 @@ using namespace std;
 
 
 int main(int argc, char** argv) {
-
+    po::variables_map var_map;
     try {
-        po::variables_map var_map = parse_args(argc, argv);
-
-        vector<Sudoku> sudokus = Sudoku::from_file("../../data/quizzes.sudoku");
-
-        for (Sudoku s: sudokus) {
-            s.solve();
-            if (var_map.count("pretty")) {
-                cout << s.to_pretty_string() << endl;
-            } else {
-                cout << s.to_string() << endl;
-            }
-
-        }
-
+        var_map = parse_args(argc, argv);
     } catch (int error) {
         return error;
+    }
+
+    vector<Sudoku> sudokus = Sudoku::from_file(var_map["input"].as<vector<string>>()[0]);
+
+    for (Sudoku s: sudokus) {
+        s.solve();
+        if (var_map.count("pretty")) {
+            cout << s.to_pretty_string() << endl;
+        } else {
+            cout << s.to_string() << endl;
+        }
+
     }
 
     return 0;
--- cpp/src/utils.cpp
+++ cpp/src/utils.cpp
@@ -2,35 +2,69 @@
 #include <vector>
 #include <fstream>
 #include <iostream>
+#include <filesystem>
 #include <boost/program_options.hpp>
 
 #include "utils.hpp"
 
 using namespace std;
 
+void print_usage(char* name, po::options_description opts_desc) {
+    cout << "Usage: " << name << " [options] <input>\n\n";
+    cout << opts_desc << "\n";
+}
+
 po::variables_map parse_args(int argc, char** argv) {
 
-    po::options_description opts_desc("Program options");
-    opts_desc.add_options()
+    po::options_description opts_desc("Allowed options");
+
+    po::options_description opts_desc_generic("Program options");
+    opts_desc_generic.add_options()
             ("help,h", "print help message and exit")
             ("pretty,p", "pretty print the output");
 
+    po::options_description opts_desc_hidden("Hidden options");
+    opts_desc_hidden.add_options()
+            ("input", po::value<vector<string>>(), "input file");
+
+    opts_desc.add(opts_desc_generic).add(opts_desc_hidden);
+
     po::positional_options_description pos_opts_desc;
-    pos_opts_desc.add("input", 1);
+    pos_opts_desc.add("input", -1);
 
     po::variables_map var_map;
     po::store(po::command_line_parser(argc, argv).options(opts_desc).positional(pos_opts_desc).run(), var_map);
     po::notify(var_map);
 
     if (var_map.count("help")) {
-        cout << "Usage: " << argv[0] << " [options] <input>\n\n";
-        cout << opts_desc << "\n";
+        print_usage(argv[0], opts_desc_generic);
         throw 1;
+    }
+
+    if (!var_map.count("input")) {
+        cout << "No input file passed!\n" << endl;
+        print_usage(argv[0], opts_desc_generic);
+        throw 2;
+    }
+
+    vector<string> input_vector = var_map["input"].as<vector<string>>();
+    if (input_vector.size() > 1) {
+        cout << "Only one input file at a time is supported!\n" << endl;
+        print_usage(argv[0], opts_desc_generic);
+        throw 3;
+    }
+
+    namespace fs = std::filesystem;
+    string file_path_string = input_vector[0];
+    fs::path file_path(file_path_string);
+    if (!fs::exists(file_path)) {
+        std::cout << "The file " << file_path_string << " does not exist, or cannot be read!\n" << endl;
+        print_usage(argv[0], opts_desc_generic);
+        throw 4;
     }
 
     return var_map;
 }
-
 
 vector<string> split(string s, string sep, bool keep_sep) {
     // adapted from https://stackoverflow.com/a/14266139
