544b6f073ac62b103559a8ab7d7b60a61c9c2759
Fynn Freyer <fynn.freyer@protonmail.com>
2022-08-12 19:26:35 +0200

    honor pretty print arg from cli

--- cpp/src/main.cpp
+++ cpp/src/main.cpp
@@ -11,18 +11,21 @@ int main(int argc, char** argv) {
 
     try {
         po::variables_map var_map = parse_args(argc, argv);
+
+        vector<Sudoku> sudokus = Sudoku::from_file("../../data/quizzes.sudoku");
+
+        for (Sudoku s: sudokus) {
+            s.solve();
+            if (var_map.count("pretty")) {
+                cout << s.to_pretty_string() << endl;
+            } else {
+                cout << s.to_string() << endl;
+            }
+
+        }
+
     } catch (int error) {
         return error;
-    }
-    
-    vector<Sudoku> sudokus = Sudoku::from_file("../../data/quizzes.sudoku");
-
-    for (Sudoku s: sudokus) {
-        cout << s.to_string() << endl;
-        s.solve();
-        cout << s.to_string() << endl;
-        cout << endl;
-
     }
 
     return 0;
